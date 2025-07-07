4e8f9abe1d098f39de50682e29666b27db1c3445
hiuphngok <phieu130425@gmail.com>
2025-07-07 21:02:50 +0700

    add pre-order infor

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -4,16 +4,18 @@ import { Nav } from 'react-bootstrap';
 function SideBar() {
   return (
     <div
-      className="text-white"
+      className="d-flex flex-column text-white"
       style={{
         backgroundColor: '#1a1a1a',
         position: 'sticky',
         top: '56px',
         height: 'calc(100vh - 56px)',
         overflowY: 'auto'
-      }}>
+      }}
+    >
       <h4 className="p-3">Menu</h4>
-      <Nav className="flex-column p-2">
+
+      <Nav className="flex-column flex-grow-1 px-2">
         <Nav.Link href="#" className="text-white">Item 1</Nav.Link>
         <Nav.Link href="#" className="text-white">Item 2</Nav.Link>
         <Nav.Link href="#" className="text-white">Item 3</Nav.Link>
@@ -21,9 +23,17 @@ function SideBar() {
         <Nav.Link href="#" className="text-white">Item 5</Nav.Link>
         <Nav.Link href="#" className="text-white">Item 6</Nav.Link>
         <Nav.Link href="#" className="text-white">Item 7</Nav.Link>
+
+        <Nav.Link
+          href="#"
+          className="text-white mt-auto px-2 py-3"
+          style={{ backgroundColor: '#111' }}
+        >
+          Get the pre-order information
+        </Nav.Link>
       </Nav>
     </div>
   );
-};
+}
 
 export default SideBar;
