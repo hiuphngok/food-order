c049d669f98cc2339858ef16d0cb72c2397d9e21
4porsche <laiphamhaininh@gmail.com>
2025-07-09 22:33:32 +0700

    feat(admin): switch entry component to Admin_dashboard

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Admin_dashboard from './Admin/Admin_dashboard';
 import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* Bỏ comment đoạn này để chạy CustomerScreen */}
+    {/* <App /> */} 
+    {/* Bỏ comment đoạn này để chạy AdminDashboard */}
+    <Admin_dashboard /> 
   </React.StrictMode>
 );
