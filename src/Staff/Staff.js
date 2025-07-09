7e9eb7e805bf2eb84c8fe2b65b298fcfa3391e74
imasing <162672203+imasing@users.noreply.github.com>
2025-07-09 21:49:58 +0700

    add Staff code to project(still on update)

---
+++ src/Staff/Staff.js
@@ -0,0 +1,29 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Row, Col } from "react-bootstrap";
+import React, { useState } from "react";
+import Content from "./components/Content";
+import Banner from "./components/Banner";
+import Header from "./components/Header";
+function Staff() {
+
+  const [filter, setFilter] = useState(null);
+
+  return (
+    <div style={{ width: '100%', height: '100%' }}>
+      <Header />
+
+      <Row>
+        <Col xs={3} className="sidebar">
+          <Banner onFilterChange={setFilter} />
+        </Col>
+        
+        <Col xs={9} className="content">
+          <Content filter={filter}/>
+        </Col>
+      </Row>
+
+    </div>
+  );
+}
+
+export default Staff;
