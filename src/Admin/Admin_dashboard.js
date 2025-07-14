23a0917f0970eafab1b4d509c4400b783fe3e4d8
4porsche <laiphamhaininh@gmail.com>
2025-07-15 00:51:55 +0700

    fix(admin): fix sidebar and dashboard

--- src/Admin/Admin_dashboard.js
+++ src/Admin/Admin_dashboard.js
@@ -1,25 +1,22 @@
 import Header from '../components/Header/Header';
-import SideBar from '../components/SideBar/SideBar';
-import DashBoard from './DashBoard';
+import Admin_SideBar from '../components/SideBar/Admin_SideBar';
 import { Row, Col, Container } from 'react-bootstrap';
+import AdminRoutes from './AdminRoutes';
 
 export default function Admin_dashboard(){
     return (
         <>
-            <Header />
-            <Container fluid>
-                <Row className="gx-0" style={{ paddingTop: '56px' }}>
-                <Col lg={2} md={3}><SideBar /></Col>
-                <Col lg={10} md={9}>
-                    <Row className="gx-3 gy-4 px-3">
-                        <DashBoard />
-                    </Row>
-                    
-                </Col>
+        <Header />
+        <Container fluid style={{ paddingTop: '56px' }}>
+            <Row>
+            <Col lg={2} md={3} className="gx-0">
+                <Admin_SideBar />
+            </Col>
+            <Col lg={10} md={9} className="offset-lg-2 offset-md-3 pt-5 px-4 gx-0">
+                <AdminRoutes />
+            </Col>
             </Row>
-            </Container>
-            
-        </>
-        
+        </Container>
+        </>       
     )
 };
