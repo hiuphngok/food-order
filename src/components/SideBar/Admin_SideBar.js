827fcabbc76efb23aebefd0e96bcc6953a296a25
4porsche <laiphamhaininh@gmail.com>
2025-07-16 03:42:26 +0700

    refactor(sidebar): modify admin sidebar

--- src/components/SideBar/Admin_SideBar.js
+++ src/components/SideBar/Admin_SideBar.js
@@ -1,25 +1,33 @@
-import { Nav } from 'react-bootstrap';
-import { Link, NavLink } from 'react-router-dom';
+import { Nav } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+import "./Admin_SideBar.css";
 
 export default function Admin_SideBar() {
   return (
-    <div
-      style={{
-        backgroundColor: '#1a1a1a',
-        color: 'white',
-        height: '100vh',
-        paddingTop: '56px',
-        position: 'fixed',
-        top: 0,
-        left: 0,
-        width: '16.6666%'
-      }}
-    >
+    <div className="admin-sidebar">
       <h4 className="p-3">Admin DashBoard</h4>
       <Nav className="flex-column px-3">
-        <Nav.Link as={NavLink} to="/admin/menu" className="text-white">Quản lý Menu</Nav.Link>
-        <Nav.Link as={NavLink} to="/admin/users" className="text-white">Quản lý Tài khoản</Nav.Link>
-        <Nav.Link as={NavLink} to="/admin/orders" className="text-white">Xem Đơn hàng</Nav.Link>
+        <Nav.Link
+          as={NavLink}
+          to="menu"
+          className="sidebar-link"
+        >
+          Quản lý Menu
+        </Nav.Link>
+        <Nav.Link
+          as={NavLink}
+          to="users"
+          className="sidebar-link"
+        >
+          Quản lý Tài khoản
+        </Nav.Link>
+        <Nav.Link
+          as={NavLink}
+          to="orders"
+          className="sidebar-link"
+        >
+          Xem Đơn hàng
+        </Nav.Link>
       </Nav>
     </div>
   );
