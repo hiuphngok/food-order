49e6eb411aff6baf0ebecec4941f5e63c07e60fe
4porsche <laiphamhaininh@gmail.com>
2025-07-16 03:35:29 +0700

    feat(src): create App.js for routing role's screen

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react";
+import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
+
+import Login from "./components/Login/Login";
+import CustomerScreen from "./Customer/CustomerScreen";
+import Staff from "./Staff/Staff";
+import Admin_dashboard from "./Admin/AdminLayout";
+import OrderManager from "./Admin/OrderManager";
+import AccountManager from "./Admin/AccountManager";
+import MenuManager from "./Admin/MenuManager";
+import DashBoard from "./Admin/DashBoard";
+
+export default function App(){
+    const [user, setUser] = useState(null);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const checkUser = () => {
+            const savedUser = JSON.parse(localStorage.getItem("user"));
+            if (savedUser) setUser(savedUser);
+        };
+
+        checkUser();
+
+        window.addEventListener("storage", checkUser);
+        return () => window.removeEventListener("storage", checkUser);
+    }, []);
+
+    if (!user) {
+    return (
+        <Routes>
+        <Route path="/login" element={<Login setUser={setUser} />} />
+        <Route path="*" element={<Navigate to="/login" />} />
+        </Routes>
+    );
+    }
+    
+    return (
+        <Routes>
+            <Route path="/login" element={<Login setUser={setUser} />} />
+            {user.roleId === 1 && (
+                <>
+                <Route path="/" element={<CustomerScreen setUser={setUser} />} />
+                <Route path="*" element={<Navigate to="/" />} />
+                </>
+            )}
+            {user.roleId === 2 && (
+                <>
+                <Route path="/staff" element={<Staff setUser={setUser} />} />
+                <Route path="*" element={<Navigate to="/staff" />} />
+                </>
+            )}
+            {user.roleId === 3 && (
+                <>
+                <Route path="/admin" element={<Admin_dashboard setUser={setUser} />}>
+                    <Route index element={<DashBoard />} />
+                    <Route path="menu" element={<MenuManager />} />
+                    <Route path="orders" element={<OrderManager />} />
+                    <Route path="users" element={<AccountManager />} />
+                </Route>
+                <Route path="*" element={<Navigate to="/admin" />} />
+                </>
+            )}
+        </Routes>
+    );
+
+}
