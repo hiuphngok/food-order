28020f2925318138c22536b43b9579c806f15f58
4porsche <laiphamhaininh@gmail.com>
2025-07-16 03:34:23 +0700

    refactor(admin): modify adminlayout

---
+++ src/Admin/AdminLayout.js
@@ -0,0 +1,23 @@
+import Admin_Header from '../components/Header/Header';
+import Admin_SideBar from '../components/SideBar/Admin_SideBar';
+import { Row, Col, Container } from 'react-bootstrap';
+import AdminRoutes from './AdminRoutes';
+import { Outlet } from 'react-router-dom';
+
+export default function Admin_dashboard({ setUser }){
+    return (
+        <>
+            <Admin_Header setUser={setUser}/>
+            <Container fluid style={{ paddingTop: '56px' }}>
+                <Row>
+                <Col lg={2} md={3} className="gx-0">
+                    <Admin_SideBar />
+                </Col>
+                <Col lg={10} md={9} className="pt-5 px-4 gx-0">
+                    <Outlet />
+                </Col>
+                </Row>
+            </Container>
+        </>  
+    )
+};
