e85def880cc5e69b744d15eadbdcba8494e72098
4porsche <laiphamhaininh@gmail.com>
2025-07-16 03:47:52 +0700

    refactor(src): use App.js for root

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {BrowserRouter} from 'react-router-dom'
 import CustomerScreen from './Customer/CustomerScreen';
 // import Admin_dashboard from './Admin/Admin_dashboard';
 import PreOrderPage from './Customer/PreOrderInfor';
 import './index.css'
+import Admin_dashboard from './Admin/AdminLayout';
+import App from './App';
 import Staff from './Staff/Staff';
 import TestRouter from './TestRouter';
 
@@ -11,8 +14,8 @@ import TestRouter from './TestRouter';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* Bỏ comment đoạn này để chạy AdminDashboard <Admin_dashboard /> */}
-    <TestRouter />
-    {/* <Staff /> */}
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
